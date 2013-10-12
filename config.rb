4b7534f20f662c394ef6cbafed5aea35e83a6b74
GCorbel <guirec.corbel@gmail.com>
2013-10-12 08:45:01 -0400

    first commit

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/blog-en/"
+http_images_path = "/blog-en/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/blog-en/fonts"
+css_dir = "public/blog-en/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
